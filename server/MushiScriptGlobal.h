504a41baf1183901bffaf367828daf406e85d571
Jordan Wambaugh <jordan@wambaugh.org>
2009-02-23 17:22:36 -0800

    added missing header file

---
+++ server/MushiScriptGlobal.h
@@ -0,0 +1,10 @@
+#ifndef MUSHISCRIPTGLOBAL_H
+#define MUSHISCRIPTGLOBAL_H
+#include <QTScript>
+#include <QScriptEngine>
+#include "utils.h"
+
+QScriptValue MushiScriptGlobalInclude(QScriptContext *ctx, QScriptEngine *eng);
+
+
+#endif // MUSHISCRIPTGLOBAL_H
